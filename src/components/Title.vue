b903a1042d4fe5fdcf64e10dd44419898018231b
markcaf <marcocafassog@gmail.com>
2022-07-15 01:43:07 +0200

    Edit component with bootstrap syntax

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,16 +1,20 @@
 <template>
-    <div class="title">
-        <h1>La mia prima app con Vue CLI!</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+            <div class="title">
+                <h1>La mia prima app con Vue CLI!</h1>
+            </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Title',
-  
-}
+  name: "Title",
+};
 </script>
 
 <style>
-
 </style>
