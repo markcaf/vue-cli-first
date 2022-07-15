ec83481db8eb281e3743098e9382375f83e5d2a4
markcaf <marcocafassog@gmail.com>
2022-07-15 02:00:13 +0200

    Bonus: created the new component Hero

---
+++ src/components/Hero.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="jumbotron">
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Hero",
+};
+</script>
+
+<style>
+    #jumbotron{
+        height: 400px;
+        background-image: url(https://www.gamesoul.it/wp-content/uploads/2020/02/Final-Fantasy-VII-Remake-nuova-key-art.jpg);
+        background-size: cover;
+    }
+</style>
